93778fa386bd918408cc7a997d111dd87663df9c
Newew44 <142455952+Newew44@users.noreply.github.com>
2023-08-22 21:46:10 -0300

    exercicio.js

---
+++ exercicio.js
@@ -0,0 +1,25 @@
+/*
+*Kauê Prestes
+*
+*/
+
+//1. a = 10  b = 5
+
+//2. a = 10  b = 10  c = 10
+
+//2. perguntaHora    perguntaSalario
+
+//4.
+
+let nome
+let idade
+console.log(typeof nome);
+console.log(typeof idade);
+// porque nao foi definido string ou number para as variaveis
+nome = prompt("Qual é o seu nome?");
+idade = prompt("Qual a sua idade?");
+
+console.log(nome);
+console.log(idade);
+// foi definido o string e o number fazendo perguntas para o usuario
+console.log("Olá", nome,"você tem", idade,"anos.")
