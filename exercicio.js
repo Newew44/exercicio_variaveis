da5936bcde03a254319e19c801513c284243aaa7
Newew44 <142455952+Newew44@users.noreply.github.com>
2023-08-24 19:54:48 -0300

    Update exercicio.js

--- exercicio.js
+++ exercicio.js
@@ -23,3 +23,32 @@ console.log(nome);
 console.log(idade);
 // foi definido o string e o number fazendo perguntas para o usuario
 console.log("Olá", nome,"você tem", idade,"anos.")
+
+//5.
+
+let roupa
+let cachorro
+let gato
+
+roupa = prompt("Você está usando uma roupa azul hoje?");
+cachorro = prompt("Você gosta de cachorros?");
+gato = prompt("Você gosta de gatos?");
+
+console.log(roupa);
+console.log(cachorro);
+console.log(gato);
+
+console.log( "Você está usando uma roupa azul hoje?", roupa);
+console.log( "Você gosta de cachorros?", cachorro);
+console.log( "Você gosta de gatos?", gato);
+
+//6.
+
+let a = 10;
+let b = 25;
+let c;
+ c = a;
+ a = b;
+ b = c;
+
+ console.log(a, b, c);
